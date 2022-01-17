48e18e561c5a666cd593e7288bf07f606952421c
Ibrahima Diallo <ibrahima.diallo1289@gmail.com>
2022-01-17 17:57:44 -0500

    bash file added

--- aws-infrastructure/ec2.tf
+++ aws-infrastructure/ec2.tf
@@ -6,8 +6,24 @@ resource "aws_instance" "appserver1" {
   availability_zone      = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
   subnet_id              = aws_subnet.application-subnet-1a.id
-  #user_data              = file("install_apache.sh")
-
+  
+  user_data = <<-EOL
+  #!/bin/bash
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    Software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  apt-cache madison docker-ce
+  sudo apt install docker.io
+  sudo apt install docker-compose
+  sudo usermod -a -G docker ubuntu
+  EOL
+  
   tags = {
     Name = "App Server 1a"
   }
