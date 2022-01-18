1d965cd34b8fea6cb079ab6f9a5dcdb6a3ca094d
unknown <jespstpierre@yahoo.com>
2022-01-17 22:54:15 -0500

    set up terr2

--- dynamodb.tf
+++ dynamodb.tf
@@ -11,15 +11,3 @@ terraform {
       dynamodb_table = "gr5-terraform-lock"
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "gr5-terraform-lock2"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
