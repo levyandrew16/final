cf2229e205eb6d0fde683d7e88fc6b10f607add2
unknown <jespstpierre@yahoo.com>
2022-01-17 22:52:44 -0500

    set up terr1

--- backend.tf
+++
@@ -1,9 +0,0 @@
-#to set up the remote backend
-terraform {
-  backend "s3"{
-      bucket = "terraform-remote-state-kuragrp5"
-      key = "inventorize/infrastructure.tfstate"
-      region = "us-east-1"
-      dynamodb_table = "gr5-terraform-lock"
-  }
-}
--- dynamodb.tf
+++ dynamodb.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#to set up the remote backend
+terraform {
+  backend "s3"{
+      bucket = "terraform-remote-state-kuragrp5"
+      key = "inventorize/infrastructure.tfstate"
+      region = "us-east-1"
+      dynamodb_table = "gr5-terraform-lock"
+  }
+}
+
 resource "aws_dynamodb_table" "terraform_locks" {
   name           = "gr5-terraform-lock2"
   billing_mode   = "PAY_PER_REQUEST"
