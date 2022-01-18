d987ba1af835c802e7a8256231517a13a1bce8e0
unknown <jespstpierre@yahoo.com>
2022-01-17 22:14:47 -0500

    setting terraform infra3

--- dynamodb.tf
+++ dynamodb.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name           = "gr5-terraform-lock"
+  name           = "gr5-terraform-lock2"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
   
